01561de0f40ecba7d074fd820ce84ef06667509d
wisrkiyr <wisrkiyr@gmail.com>
2022-04-05 11:41:39 +0900

    mdfy

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import myTools as mts
 
 if __name__ == '__main__':
     cvseg = ChanVese(
-        N=4, nu=5, dt=.3, tol=1E-03,
+        N=4, nu=5, dt=.2, tol=1E-03,
         method='vector', initial=None,
         # method='gray', initial=None,
         reinterm=10, vismode=True, visterm=50
@@ -17,16 +17,17 @@ if __name__ == '__main__':
 
     dir_save = './results/'
     nm_imgs = ['000000046048']
+    nm_imgs = ['flowers']
 
     for nm_img in nm_imgs:
         try:
             img0 = plt.imread(f'./data/{nm_img}.jpg')
         except FileExistsError:
             img0 = plt.imread(f'./data/{nm_img}.png')
-        img = mts.gaussfilt(img0, sig=1)
+        # img = mts.gaussfilt(img0, sig=1)
 
         sts = mts.SaveTools(join(dir_save, nm_img))
-        pc_img, phis = cvseg.segmentation(img)
+        pc_img, phis = cvseg.segmentation(img0)
 
         mts.makeDir(join(dir_save, nm_img))
         sts.imshow(img0, 'input')
